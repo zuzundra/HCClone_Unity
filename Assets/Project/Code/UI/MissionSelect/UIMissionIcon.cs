410164fbfe304118267d3957e9d0c8a850bdd31b
zuzundra <knitted.games@yandex.ru>
2015-11-14 12:07:09 +0300

    1
    1

---
+++ Assets/Project/Code/UI/MissionSelect/UIMissionIcon.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[RequireComponent(typeof(Button))]
+public class UIMissionIcon : MonoBehaviour {
+	[SerializeField]
+	private EMissionKey _missionKey = EMissionKey.None;
+	private EPlanetKey _planetKey = EPlanetKey.None;
+	
+	public void Start() {
+#if UNITY_EDITOR && MISSIONS_TEST
+		if (!Global.IsInitialized) {
+			Global.Instance.Initialize();
+		}
+#endif
+		
+		PlanetData pd = MissionsConfig.Instance.GetPlanet(_missionKey);
+		if (pd != null) {
+			_planetKey = MissionsConfig.Instance.GetPlanet(_missionKey).Key;
+		}
+
+		bool missionAvailable = false;
+
+		if (_planetKey != EPlanetKey.None && _missionKey != EMissionKey.None) {
+			if (Global.Instance.Player.StoryProgress.IsMissioAvailable(_planetKey, _missionKey)) {
+				missionAvailable = true;
+			}
+		}
+
+		if (missionAvailable) {
+			gameObject.GetComponent<Button>().onClick.AddListener(OnIconClick);
+		} else {
+			gameObject.GetComponent<Button>().interactable = false;
+		}
+	}
+
+	private void OnIconClick() {
+		if (_missionKey != EMissionKey.None) {
+			UIWindowBattlePreview wbp = UIWindowsManager.Instance.GetWindow(EUIWindowKey.BattlePreview) as UIWindowBattlePreview;
+			wbp.Show(_planetKey, _missionKey);
+		}
+	}
+}
---
+++ Assets/Project/Code/UI/Windows/UIUnitSelectInfo.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIUnitSelectInfo : MonoBehaviour
+{
+    public event EventHandler UnitIsConfirmed;
+
+    BaseSoldierData _unitData = null;
+    public BaseSoldierData UnitData
+    {
+        get
+        {
+            return _unitData;
+        }
+    }
+
+    Button _button = null; 
+    public Button Button
+    {
+        get
+        {
+            if (_button == null)
+                _button = gameObject.GetComponent<Button>();
+            return _button;
+        }
+    }
+
+    public void Awake()
+    {
+        Button.image.enabled = false;
+        Button.onClick.AddListener(OnBtnClick);
+    }
+
+    public void LoadSoldierData(BaseSoldierData unitData, bool isSelected)
+    {
+        _unitData = unitData;
+        if (_unitData == null)
+            return;
+
+        Button button = Button;
+        GameObject cardResource = UIResourcesManager.Instance.GetResource<GameObject>(
+            string.Format("{0}/{1}", GameConstants.Paths.UNIT_PREFAB_RESOURCES, _unitData.CardPrefab));
+        GameObject cardUnitData = GameObject.Instantiate(cardResource) as GameObject;
+        cardUnitData.transform.SetParent(button.transform, false);
+
+        RectTransform rectCard = cardUnitData.GetComponent<RectTransform>();
+        Rect rectImage = button.image.rectTransform.rect;
+        rectCard.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rectImage.width);
+        rectCard.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rectImage.height);
+        rectCard.anchoredPosition = new Vector2(rectImage.width / 2, -rectImage.height / 2);
+
+        (gameObject.GetComponent<MultiImageButton>()).AddChildImages(cardUnitData);
+    }
+
+    private void OnBtnClick()
+    {
+        if (_unitData != null)
+        {
+            UIWindowUnitConfirm wuc = UIWindowsManager.Instance.GetWindow(EUIWindowKey.UnitConfirm) as UIWindowUnitConfirm;
+            wuc.UnitIsConfirmed += new System.EventHandler(wuc_UnitIsConfirmed);
+            wuc.ConfirmIsHided += new System.EventHandler(wuc_ConfirmIsHided);
+            wuc.Show(_unitData);
+        }
+    }
+
+    void wuc_UnitIsConfirmed(object sender, System.EventArgs e)
+    {
+        if (UnitIsConfirmed != null)
+            UnitIsConfirmed(this, e);
+    }
+
+    void wuc_ConfirmIsHided(object sender, System.EventArgs e)
+    {
+        UIWindowUnitConfirm wuc = (UIWindowUnitConfirm)sender;
+        wuc.UnitIsConfirmed -= new System.EventHandler(wuc_UnitIsConfirmed);
+        wuc.ConfirmIsHided -= new System.EventHandler(wuc_ConfirmIsHided);
+    }
+
+    public void ClearData()
+    {
+        Button button = Button;
+        for (int i = button.transform.childCount; i > 0; i--)
+            button.transform.GetChild(i - 1).SetParent(null);
+
+        _unitData = null;
+    }
+}
